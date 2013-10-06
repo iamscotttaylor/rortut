8a828d25d55dae2850516635322f67fa93353191
Scott Taylor <scott@staylor.st>
2013-10-06 10:29:06 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rortut::Application.config.secret_token = '1e9807a72ed4e99d9f7253b6a1c17136ae544c6a648353827c30b2a04147aa0f1e3d91549228cc71de2b105c9ac02fe9162109cfa6ba169932187d4a21e78305'
